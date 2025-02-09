8e6a8cb923011ee02d6e7519a54ab53e3d0dd816
Buphaa <buffaian17@gmail.com>
2025-02-09 17:50:39 -0300

    creamos todos los componentes que creemos necesarios en la pagina

---
+++ src/app/perfume-detalles/perfume-detalles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfume-detalles',
+  imports: [],
+  templateUrl: './perfume-detalles.component.html',
+  styleUrl: './perfume-detalles.component.css'
+})
+export class PerfumeDetallesComponent {
+
+}
