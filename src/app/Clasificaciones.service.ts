6653ae67137d6bc8cf3b064fcdf6fe1fea0eccad
Buphaa <buffaian17@gmail.com>
2025-02-10 16:54:33 -0300

    ya funcionan la clasificaciones de los perfumes, con la individual y la general

--- src/app/Clasificaciones.service.ts
+++ src/app/Clasificaciones.service.ts
@@ -11,4 +11,24 @@ import { Perfume } from './Perfume';
   })
 
   export class ClasificacionesService {
+    constructor(private http: HttpClient) { }
+
+    getOneClasificacion(idPerfume: number): Observable<any> {
+        const token = localStorage.getItem('token');
+        const headers = { Authorization: `Bearer ${token}` };
+        return this.http.get<any>('http://localhost:3000/clasificaciones/'+ idPerfume, {headers});
+
+    }
+
+    getAllClasificaciones(idPerfume: number): Observable<any> {
+        const token = localStorage.getItem('token');
+        const headers = { Authorization: `Bearer ${token}` };
+        return this.http.get<any[]>('http://localhost:3000/clasificaciones/all/'+ idPerfume, {headers});
+    }
+
+    addClasificacion(idPerfume: number, idClasificacion: number): Observable<any> {
+        const token = localStorage.getItem('token');
+        const headers = { Authorization: `Bearer ${token}` };
+        return this.http.post('http://localhost:3000/clasificaciones/add', {idPerfume, idClasificacion}, {headers});
+    }
   }
